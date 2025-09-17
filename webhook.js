7d0da9635a1091f288348a8784579a8485ff1c75
Yoji Macadangdang <yojimacadangdang@gmail.com>
2025-09-17 17:12:50 +0800

    update

--- webhook.js
+++ webhook.js
@@ -3,7 +3,7 @@ const app = express();
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN ||  'chatbotverify';
 
-app.get('/webhook', (req, res) => {
+app.get('/api/webhook', (req, res) => {
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
