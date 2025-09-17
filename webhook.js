55f7a99a2fbd963dee597d679a099ac09b173f25
Yoji Macadangdang <yojimacadangdang@gmail.com>
2025-09-17 16:43:03 +0800

    initial commit

---
+++ webhook.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN ||  'TOKEN_NAME';
+
+app.get('/api/webhook', (req, res) => {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token == VERIFY_TOKEN){
+        console.log('Webhook verified');
+        return res.status(200).send(challenge);
+    } else {
+        console.warn('Verification failed. Token mismatch!');
+        return res.sendStatus(403);
+    }
+});
+
+module.exports = app;
